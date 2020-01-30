4347da8f26369981a461b3cab27e609937b60641
Robert Balaban <rob.balaban@gmail.com>
2020-01-30 00:18:52 +0000

    Adds rgb to hex transformer

--- helpers/transformers.js
+++ helpers/transformers.js
@@ -1,6 +1,24 @@
 /**
  *
  * @param {Object} color
+ * Takes in Object of RGB returns the Hex value of the color
+ */
+
+//TODO: Deal with RGB(A) alpha
+
+const transformToRGB = color => {
+  let rgb = ["#"];
+  Object.keys(color).forEach(key =>
+    if (key === 'a') return;
+    rgb.push(color[key].toString(16).slice(-2))
+  );
+  return rgb.join("");
+};
+
+
+/**
+ *
+ * @param {Object} color
  * Takes in Object of RGB returns Object with values 0-1
  */
 
